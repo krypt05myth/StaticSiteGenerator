1827f42c72fabfd3498cc6bde9de1160bec1f8c3
krypt0l5mythe <krypt0l5mythe@krypt0l5mythe.rocks>
2026-01-12 00:17:17 -1000

    Updated inline_markdown with text_to_textnodes, updated test suite

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -89,6 +89,15 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_nodes_splitted.append(TextNode(temp[i], text_type))
     return new_nodes_splitted
 
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
 ## FOR INTERNAL-TO-FILE TESTING
 # if __name__ == "__main__":
 #     node = TextNode("This is text with a `code block` word", TextType.TEXT)
--- src/test_inline_md.py
+++ src/test_inline_md.py
@@ -6,6 +6,7 @@ from inline_markdown import (
     split_nodes_delimiter,
     split_nodes_image,
     split_nodes_link,
+    text_to_textnodes
 )
 
 class TestExtractors(unittest.TestCase):
@@ -118,6 +119,26 @@ class TestInlineMarkdown(unittest.TestCase):
             ],
             new_nodes,
         )
+
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            nodes,
+        )
+        
 class TestHTMLNode(unittest.TestCase):
     def test_split_bold(self): 
         node = TextNode("This is text with a **BOLD** word.", TextType.TEXT)
